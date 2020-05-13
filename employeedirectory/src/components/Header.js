bde21ff10e46bcccfe70d16995e3cbded6355980
thip <thipsavanh@gmail.com>
2020-05-13 02:50:57 -0500

    created header.js

---
+++ employeedirectory/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Employee Directory</h1>
+                <p className="lead">Click on name to filter directory by employee names.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
