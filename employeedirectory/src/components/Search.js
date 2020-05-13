b81b80014e03ae317bd9da0b74015aabc6bb54e2
thip <thipsavanh@gmail.com>
2020-05-13 02:51:26 -0500

    created search.js

---
+++ employeedirectory/src/components/Search.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Search(props) {
+    return (
+      <form>
+        <div className="form-group">
+          <input
+            onChange={props.handleInputChange}
+            onClick={props.handleFormSubmit}
+            value={props.search}
+            name="search"
+            type="text"
+            className="form-control"
+            placeholder="Search"
+            id="search"
+          />
+        </div>
+      </form>
+    );
+  }
+
+export default Search;
